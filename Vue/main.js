1494060b41f9857190009b38531a36632b7ceb31
Jesper Gustafsson <jesper256@gmail.com>
2018-04-05 22:19:49 +0200

    .

--- Vue/main.js
+++ Vue/main.js
@@ -1,8 +1,20 @@
+
+Vue.component('button-counter', {
+    data: function () {
+      return {
+        likes: 0,
+        dislikes: 0,
+      }
+    },
+    template: '<div><button v-on:click="likes++">Likes {{ likes }} </button><button v-on:click="dislikes++">Dislikes {{ dislikes }} </button></div>'
+  });
+
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue! ' + new Date().toLocaleString(),
+        message: 'Hello super mega cool cat! ' + new Date().toLocaleString(),
         catNamePrefix: "Cool ",
+        showCats: true,
         cats: [
             { name: 'Cool cat', id: 0, prefix: this.catNamePrefix },
             { name: 'Yellow Vue cat', id: 1, prefix: this.catNamePrefix },
@@ -18,3 +30,4 @@ var app = new Vue({
         }
     }
 });
+
