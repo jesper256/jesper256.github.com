6fbe26c7a0f2a698bbc213b8745188a38332f4a7
Jesper Gustafsson <jesper256@gmail.com>
2018-04-05 20:01:43 +0200

    test commit via VS Code

---
+++ Vue/main.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue! ' + new Date().toLocaleString(),
+        catNamePrefix: "Cool ",
+        cats: [
+            { name: 'Cool cat', id: 0, prefix: this.catNamePrefix },
+            { name: 'Yellow Vue cat', id: 1, prefix: this.catNamePrefix },
+            { name: 'No cat at all', id: 2, prefix: this.catNamePrefix }
+        ]
+    },
+    methods: {
+        addCat: function () {
+            this.cats.push({ name: this.catNamePrefix + 'cat #' + this.cats.length, id: this.cats.length, prefix: this.catNamePrefix });
+        },
+        removeCat: function (cat) {
+            this.cats = this.cats.filter(function (c) { return c.id !== cat.id });
+        }
+    }
+});
